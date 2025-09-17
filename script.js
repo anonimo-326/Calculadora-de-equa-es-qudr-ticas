21a16c87439076f35aebd306377fe8d9c6af5afa
anonimo-326 <castroluanleite@gmail.com>
2025-09-16 21:54:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function calcular(){
+    let a = parseFloat(document.getElementById('a').value);
+    let b = parseFloat(document.getElementById('b').value);
+    let c = parseFloat(document.getElementById('c').value);
+    let delta = b**2-4*a*c
+    if (delta >= 0){
+        let x1 = ((b*(-1))+Math.sqrt(delta))/(2*a);
+        let x2 = ((b*(-1))-Math.sqrt(delta))/(2*a);
+        document.getElementById('label').innerHTML = `x = {${x1}, ${x2}}`;
+    }
+    else{
+        document.getElementById('label').innerHTML = 'A equação não tem resolução.';
+    }
+}
