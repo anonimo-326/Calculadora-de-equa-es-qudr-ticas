9258a049397244d3b51ef25ef185e55535cb6e44
anonimo-326 <castroluanleite@gmail.com>
2025-09-17 12:05:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ function calcular(){
     if (delta >= 0){
         let x1 = ((b*(-1))+Math.sqrt(delta))/(2*a);
         let x2 = ((b*(-1))-Math.sqrt(delta))/(2*a);
-        document.getElementById('label').innerHTML = `x = {${x1}, ${x2}}`;
+        if (x1 < x2){
+            document.getElementById('label').innerHTML = `x = {${x1}, ${x2}}`;
+        }
+        if (x1 > x2){
+            document.getElementById('label').innerHTML = `x = {${x2}, ${x1}}`;
+        }
+        else{
+            document.getElementById('label').innerHTML = `x = ${x1||x2}`
+        }
     }
     else{
         document.getElementById('label').innerHTML = 'A equação não tem resolução.';
